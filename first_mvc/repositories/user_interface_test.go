1963859f0b1870001e96a47a0155b231e41c0ef3
ttnny <ttnny@users.noreply.github.com>
2020-01-15 04:01:59 -0800

    Update MVC example

--- first_mvc/repositories/user_interface_test.go
+++ first_mvc/repositories/user_interface_test.go
@@ -1,12 +1,19 @@
 package repositories
 
-import "testing"
+import (
+	"net/http"
+	"testing"
+)
 
 func TestGetUserNoUserFound(t *testing.T) {
 	user, err := GetUser(0)
 
 	if user != nil {
 		t.Error("Not expecting a user with ID = 0")
+	}
+
+	if err.StatusCode != http.StatusNotFound {
+		t.Error("Expecting 404 when user not found")
 	}
 
 	if err == nil {
